639c89e7e33c1afd1a1fc64f762f808668ed861d
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-02-27 16:22:34 +0200

    small modifications

--- src/app/department/department-list/department-list.component.ts
+++ src/app/department/department-list/department-list.component.ts
@@ -5,31 +5,30 @@ import { Department } from 'src/app/models/department';
 @Component({
   selector: 'app-department-list',
   templateUrl: './department-list.component.html',
-  styleUrls: ['./department-list.component.css']
+  styleUrls: ['./department-list.component.css'],
 })
 export class DepartmentListComponent implements OnInit {
-  departments:Department[]=[];
-  constructor(private deptService:DepartmentService) { }
+  departments: Department[] = [];
+  constructor(private deptService: DepartmentService) {}
   cropWidth = 75;
   flag = false;
-  changeCropWidth(dept:Department){
-    this.cropWidth = dept.rating*75/5;
+  changeCropWidth(dept: Department) {
+    this.cropWidth = (dept.rating * 75) / 5;
   }
 
-  deleteRow(deptId:number){
+  deleteRow(deptId: number) {
     this.deptService.deleteDepartmentById(deptId);
   }
-  editRow(){
+  editRow() {
     //this.deptService.editDepartmentData();
-    this.flag=!this.flag;
+    this.flag = !this.flag;
     //this.deptService.editDepartmentData(dept);
   }
-  getDataFromChild(e:any){
+  getDataFromChild(e: any) {
     console.log(e);
     this.deptService.editDepartmentData(e);
   }
   ngOnInit(): void {
-    this.departments=this.deptService.getAllDepartments();
+    this.departments = this.deptService.getAllDepartments();
   }
-
 }
